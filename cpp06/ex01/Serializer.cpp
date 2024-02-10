9a2a9fc002535c059814c49d9844317094ff6658
Nikita Ovsiannik <novsiann@c2s2d4.42wolfsburg.de>
2024-02-10 20:57:21 +0100

    ex01 added but not finished

---
+++ cpp06/ex01/Serializer.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: novsiann <novsiann@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 20:10:58 by novsiann          #+#    #+#             */
+/*   Updated: 2024/02/10 20:46:41 by novsiann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+
+Serializer::Serializer()
+{
+	std::cout << "Serializer default constructor is called" << std::endl;
+}
+
+Serializer::~Serializer()
+{
+	std::cout << "Serializer destructor is caled" << std::endl;
+}
+
+Serializer::Serializer( Serializer const &src )
+{
+	std::cout << "Serializer copy constructor is caled" << std::endl;
+	*this = src;
+}
+
+Serializer &Serializer::operator=(Serializer const &src )
+{
+	std::cout << "Serializer copy assignment is caled" << std::endl;
+	(void)src;
+	return (*this);
+}
+
+// uintptr_t serialize( Data *ptr )
+// {
+
+// }
+
+// Data* deserialize( uintptr_t raw )
+// {
+
+// }
---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: novsiann <novsiann@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 16:07:43 by novsiann          #+#    #+#             */
+/*   Updated: 2024/02/10 20:46:31 by novsiann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_H
+# define SERIALIZER_H
+
+#include <string>
+#include <iostream>
+
+struct Data
+{
+	int num;
+	std::string str;
+};
+
+class Serializer{
+	private:
+		Serializer();
+		~Serializer();
+		Serializer( Serializer const &src );
+		Serializer	&operator=( Serializer const &src );
+	// public:
+	// 	static uintptr_t serialize( Data* ptr );
+	// 	static Data* deserialize( uintptr_t raw );
+};
+
+#endif
