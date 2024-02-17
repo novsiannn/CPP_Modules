073246c17300731f8cfcb0d8c26f5f431e3e375a
novsiann <nikitaovsyannik77@gmail.com>
2024-02-17 11:44:26 +0100

    ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: nikitos <nikitos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 20:38:21 by nikitos           #+#    #+#             */
-/*   Updated: 2024/02/15 22:36:39 by nikitos          ###   ########.fr       */
+/*   Updated: 2024/02/17 10:38:01 by nikitos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,39 @@
 #include <string>
 #include <iostream>
 
+template<typename T>
 class Array
 {
 	public:
 		Array();
+		Array(unsigned int n);
 		~Array();
 		Array(Array const &src);
 		Array &operator=(Array const &src);
+	private:
+		T				*_arr;
+		unsigned int	_n;
+};
+
+template<typename T>
+Array<T>::Array() : _arr(NULL), _n(0)
+{
+	std::cout << "Default constructor is called." << std::endl; 
+};
+
+template<typename T>
+Array<T>::Array(unsigned int n) : _n(n)
+{
+	if (_arr)	
+		delete _arr;
+	_arr = new T(n);
+	std::cout << "Constructor is called. Length of array is " << n << std::endl; 
+};
+
+template<typename T>
+Array<T>::~Array()
+{
+	std::cout << "Default destructor is called." << std::endl; 
 };
 
 #endif
