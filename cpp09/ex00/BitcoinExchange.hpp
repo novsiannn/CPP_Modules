3b241278aa153009a41113aa81158c8d89e5264f
Nikita Ovsiannik <novsiann@c2r11s2.42wolfsburg.de>
2024-02-25 20:45:38 +0100

    cpp09 started

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: novsiann <novsiann@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 20:38:21 by nikitos           #+#    #+#             */
+/*   Updated: 2024/02/25 20:23:17 by novsiann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_H
+# define BITCOINEXCHANGE_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <deque>
+
+
+
+#endif
